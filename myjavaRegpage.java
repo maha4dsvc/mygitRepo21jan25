6a85c9bc25960f554d22bf5a96980962165b48f1
maha <maha@gmail.com>
2025-01-27 19:23:40 +0530

    dev task completed

--- myjavaRegpage.java
+++ myjavaRegpage.java
@@ -4,3 +4,8 @@ this is my java reg page
 3rd line of java code by Dev A
 
 the manager work completed
+1st day of java  code completed
+2nd day of java  code completed
+3rd day of java  code completed
+4th day of java  code completed 
+5th day of java  code completed 
